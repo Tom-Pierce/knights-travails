48ede6f27540fe9534af5a2d6b480017370670c1
Tom Pierce <tompierce4@gmail.com>
2023-09-19 11:38:25 +0100

    Move knightMoves to index and show the path the knight takes

--- src/get-shortest-knight-path.js
+++ src/get-shortest-knight-path.js
@@ -1,14 +1,13 @@
 import { samePos } from "./chess-board";
 import { chessSquare } from "./chess-square";
 
-const getShortestKnightPath = (end, current, queue = [current]) => {
+export const getShortestKnightPath = (end, current, queue = [current]) => {
   if (samePos(current.position, end)) {
     const path = [current.position];
     while (current.parent) {
       path.push(current.parent.position);
       current = current.parent;
     }
-    console.log(path);
     return path;
   }
   current.createMovesArray();
@@ -17,10 +16,5 @@ const getShortestKnightPath = (end, current, queue = [current]) => {
     queue.push(square);
   });
   queue.shift();
-  getShortestKnightPath(end, queue[0], queue);
-};
-
-export const knightMoves = (end, start) => {
-  const startSquare = chessSquare(start, null);
-  return getShortestKnightPath(end, startSquare);
+  return getShortestKnightPath(end, queue[0], queue);
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import { createBoard } from "./chess-board";
+import { chessSquare } from "./chess-square";
 import { Knight } from "./knight";
-import { knightMoves } from "./knight-moves";
+import { getShortestKnightPath } from "./get-shortest-knight-path";
 import "./style.css";
 
 createBoard();
 const knight = Knight();
 knight.move(7, 7);
+
+export const knightMoves = (end, start) => {
+  const startSquare = chessSquare(start, null);
+  const path = getShortestKnightPath(end, startSquare);
+  path.reverse();
+  path.forEach((move, index) => {
+    setTimeout(
+      () => {
+        knight.move(path[index][0], path[index][1]);
+      },
+      1000 + 1000 * index,
+    );
+  });
+};
+
 knightMoves([0, 7], [knight.x, knight.y]);
